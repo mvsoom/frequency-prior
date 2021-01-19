08a45c77d5fd4e12bcd7069902341b9c1ddd576b
marnix <mvsoom@gmail.com>
2021-01-19 18:28:55 +0100

    Finish improved driver

--- py/uninformative/driver.py
+++ py/uninformative/driver.py
@@ -16,7 +16,9 @@
 
 # # Driver script
 #
-# - Original code contained serious errors. See `errors.md`.
+# - The new prior helped uncover a fifth frequency. F1 has been resolved into two frequencies with overwhelming evidence. The reason we did not see this is because of non-overlapping intervals with the old prior, which precluded F2 getting near F1.
+#
+# - Original code contained serious errors which, strangely, did not affect the results that much. See `errors.md`.
 #
 # - The prior bounds for $\alpha$ and $\omega$ were chosen given Praat's estimates, so we choose the hyperparameters given that we know that it was `/ae/`. From Vallee (1994) we get: $\langle \boldsymbol x \rangle = (610, 1706, 2450, 3795)$. We then round these numbers. The lowerbound $x_0$ is chosen as the lower bound of $F_1$'s interval used in the paper.
 #
@@ -29,6 +31,33 @@
 # - Try wider bounds with actual overlap to see what the effect is.
 #
 # - We could compare our prior to a range of nested sampling runs with lognormals as priors with different relative uncertainty $\rho$ and then see to roughly which value of $\rho$ our prior corresponds by comparing the information (i.e. similar information as our prior for $\rho = \rho^*$ $\rightarrow$ our prior is like $\rho^*\%$ uncertainty).
+#
+# - We can show some transfer functions with error margins (GP style) as a condensed way to visualize the estimated frequencies.
+#
+# ## Nested sampling options
+#
+# The type of bounding and sampling methods used are *crucial*.
+#
+# The likelihood function with the new prior exhibits several sharp peaks within each frequency.  
+# Nested sampling performance is improved (and indeed actually finishes runs) by:  
+#
+# - Using 'multi' as the bound method -- is the only viable alternative  
+# - Using random walk 'rslice' instead of uniform sampling. The order of preference of sampling  
+#   methods is: 'unif' < 'slice' ~ 'hslice' < 'rstagger' < 'rwalk' < 'rslice'
+#   
+# - Note that `rslice` is similar to Galilean Monte Carlo [@Speagle2019]
+#
+# - Using bootstrapping to estimate expansion factor instead of the fixed default.  
+#        
+#   > Bootstrapping these expansion factors can help to ensure accurate evidence estimation    
+#   > when the proposals rely heavily on the size of an object rather than the overall shape,  
+#   > such as when proposing new points uniformly within their boundaries. In theory, it also  
+#   > helps to prevent mode "death": if occasionally a secondary mode disappears when bootstrapping,  
+#   > the existing bounds would be expanded to theoretically encompass it. In practice, however,
+#   > most modes are widely separated, [as in our case] leading enormous expansion factors  
+#   > whenever any possible instance of mode death may occur.[1]  
+#  
+# [1]: https://github.com/joshspeagle/dynesty/blob/master/demos/Examples%20--%20Gaussian%20Shells.ipynb 
 
 # +
 # %pylab inline
@@ -40,15 +69,19 @@ import analyze
 # +
 # %run driver.ipy
 
-PQ_grid = get_PQ_grid()
-data = get_data()
+PQ_grid = get_PQ_grid(5, 10)
+data = get_data('bdl/arctic_a0017', 11000)
 hyper = get_hyperparameters()
 
 # +
+# The all-important parameters for nested sampling
+samplerargs = {'nlive': 500, 'bound': 'multi', 'sample': 'rslice', 'bootstrap': 10}
+runargs = {'save_bounds': False}
+
 # Output to stdout is not printed in the notebook. If this is desired,
 # change backend to "multiprocessing" and uncomment the decorator
 # @memory.cache of model.run_nested()
-options = dict(n_jobs=7, verbose=50, timeout=None, backend=None)
+options = dict(n_jobs=6, verbose=50, timeout=60*60*2, backend=None)
 
 runid = 0
 
@@ -58,6 +91,3 @@ with joblib.Parallel(**options) as parallel:
             new, order, data, hyper, runid=runid
         ) for order in PQ_grid for new in (False, True)
     )
-# -
-
-
