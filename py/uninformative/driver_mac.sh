22f369f1b178e1d70596e9846b8c5ce14c96ea8f
marnix <mvsoom@gmail.com>
2021-01-20 16:40:35 +0100

    Add pc and mac driver scripts

---
+++ py/uninformative/driver_mac.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+python3 driver_script.py "rms/arctic_a0382" 2>/dev/null
+python3 driver_script.py "slt/arctic_b0041" 2>/dev/null
---
+++ py/uninformative/driver_pc.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+python3 driver_script.py "awb/arctic_a0094" 2>/dev/null
+python3 driver_script.py "bdl/arctic_a0017" 2>/dev/null
+python3 driver_script.py "jmk/arctic_a0067" 2>/dev/null
