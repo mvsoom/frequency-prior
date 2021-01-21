3c997f12c9e665eab257ed4f1a71ba8f79d90b99
marnix <mvsoom@gmail.com>
2021-01-21 11:55:48 +0100

    Fix bug

--- py/uninformative/transfer_cache_mac.sh
+++ py/uninformative/transfer_cache_mac.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-scp -r marnix@ai31.vub.ac.be:/Users/marnix/formant-prior/py/uninformative/cache cache
-
+remote_root=/Users/marnix/formant-prior/py/uninformative
+local_root=$WRK/proj/formant-prior/research/py/uninformative
+scp -r marnix@ai31.vub.ac.be:$remote_root/cache $local_root
--- py/uninformative/transfer_cache_pc.sh
+++ py/uninformative/transfer_cache_pc.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-scp -r marnix@ai30.vub.ac.be:/home/marnix/formant-prior/py/uninformative/cache cache
-
+remote_root=/home/marnix/formant-prior/py/uninformative
+local_root=$WRK/proj/formant-prior/research/py/uninformative
+scp -r marnix@ai30.vub.ac.be:$remote_root/cache $local_root
