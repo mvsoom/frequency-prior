7f1a90ee8f62582e8042fe9f1aee965a936a38fb
marnix <mvsoom@gmail.com>
2021-01-22 20:16:42 +0100

    Add local driver script ensuing problems merging caches across machines

---
+++ py/uninformative/driver_local.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+python3 driver_script.py "awb/arctic_a0094" 2>/dev/null
+python3 driver_script.py "bdl/arctic_a0017" 2>/dev/null
+python3 driver_script.py "jmk/arctic_a0067" 2>/dev/null
+python3 driver_script.py "rms/arctic_a0382" 2>/dev/null
+python3 driver_script.py "slt/arctic_b0041" 2>/dev/null
