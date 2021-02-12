6ef0b76cd150def1d27057f124d2e0fff340bdd4
marnix <mvsoom@gmail.com>
2021-02-12 18:54:21 +0100

    Move markdown files into md/

--- py/uninformative/transfer_cache_mac.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-remote_root=/Users/marnix/formant-prior/py/uninformative
-local_root=$WRK/proj/formant-prior/research/py/uninformative
-scp -r marnix@ai31.vub.ac.be:$remote_root/cache $local_root
--- py/uninformative/transfer_cache_pc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-remote_root=/home/marnix/formant-prior/py/uninformative
-local_root=$WRK/proj/formant-prior/research/py/uninformative
-scp -r marnix@ai30.vub.ac.be:$remote_root/cache $local_root
