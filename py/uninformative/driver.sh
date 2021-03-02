e87816cf92598fd313161c8b8bea92170f0782c0
marnix <mvsoom@gmail.com>
2021-03-02 14:31:38 +0100

    Fix bug

--- py/uninformative/driver.sh
+++ py/uninformative/driver.sh
@@ -11,9 +11,10 @@ python3 driver_script.py "jmk/arctic_a0067" hyper_free 2>/dev/null
 python3 driver_script.py "rms/arctic_a0382" hyper_free 2>/dev/null
 python3 driver_script.py "slt/arctic_b0041" hyper_free 2>/dev/null
 
+ # Hyper files must contain "hyper"; all other files are input files
 python3 post_script.py \
-    hyper_cmp hyper_free \ # Hyper files must contain "hyper"
-    "awb/arctic_a0094" \   # All other files are input files
+    hyper_cmp hyper_free \
+    "awb/arctic_a0094" \
     "bdl/arctic_a0017" \
     "jmk/arctic_a0067" \
     "rms/arctic_a0382" \
