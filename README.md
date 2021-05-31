84d4726b75739a6fe6c9dabd73fb322698e7088a
marnix <mvsoom@gmail.com>
2021-05-31 23:07:34 +0200

    Tidy up

---
+++ README.md
@@ -0,0 +1,24 @@
+% Git repo for "A Weakly Informative Prior for Resonance Frequencies"
+
+This repo contains the full code used for Experiments I and II in Section 5.
+
+## Notebooks
+
+Details of the Experiments:
+
+    - `post_cmp.ipynb`: Experiment I
+    - `post_free.ipynb`: Experiment II
+
+The five waveforms used for the Experiments are visualized in
+`show_data.ipynb`. The data itself is contained in `./arctic`.
+
+Sampling of $\pi_3$ is illustrated in `sample.ipynb`.
+
+## Source code
+
+`paretochain.py` contains code to sample from $\pi_3$ (which is the "Pareto
+chain prior").
+
+## `./post` folder
+
+Contains post-processing of the Experimental results.
--- driver.sh
+++ driver.sh
@@ -11,17 +11,9 @@ python3 driver_script.py "jmk/arctic_a0067" hyper_free 2>/dev/null
 python3 driver_script.py "rms/arctic_a0382" hyper_free 2>/dev/null
 python3 driver_script.py "slt/arctic_b0041" hyper_free 2>/dev/null
 
-# These results are not used in final analysis, so following lines
-# may be commented out
-python3 driver_script.py "awb/arctic_a0094" hyper_free12 2>/dev/null
-python3 driver_script.py "bdl/arctic_a0017" hyper_free12 2>/dev/null
-python3 driver_script.py "jmk/arctic_a0067" hyper_free12 2>/dev/null
-python3 driver_script.py "rms/arctic_a0382" hyper_free12 2>/dev/null
-python3 driver_script.py "slt/arctic_b0041" hyper_free12 2>/dev/null
-
  # Hyper files must contain "hyper"; all other files are input files
 python3 post_script.py \
-    hyper_cmp hyper_free hyper_free12 \
+    hyper_cmp hyper_free \
     "awb/arctic_a0094" \
     "bdl/arctic_a0017" \
     "jmk/arctic_a0067" \
